4afcb806aa38941bb67d919c3e3c0d48a81be0ef
Camilo Nossa <calejandro_noss@outlook.com>
2024-09-16 19:19:43 -0500

    🔥Remove APIs route handlers

--- src/app/api/roles/route.ts
+++
@@ -1,9 +0,0 @@
-import { sql } from '@vercel/postgres'
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  const { rows } = await sql`SELECT * FROM public.role`
-  return NextResponse.json(rows, {
-    status: 200
-  })
-}
